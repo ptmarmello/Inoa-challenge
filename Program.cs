2455a8bd64ca1bf848c5bb1cf70f683bf535092b
Pedro Thiago <ptmarmello@gmail.com>
2022-08-08 21:17:39 -0300

    Challenge v0 - C# app init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ 
+namespace ConsoleApp
+{
+    class Program
+    {        static void Main(string[] args)
+        {
+
+            
+
+            // Console.WriteLine("\nMy Console App!\n");
+            // var key = Console.Read();
+            // Console.WriteLine(key.ToString());
+        }
+    }
+}
