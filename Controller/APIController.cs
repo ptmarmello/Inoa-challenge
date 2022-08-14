d201ae9152e4ba24213c46f4f0bfa40ba8938f80
Pedro Thiago <ptmarmello@gmail.com>
2022-08-14 01:13:03 -0300

    Reestructuring Controller namespace

--- Controller/APIController.cs
+++ Controller/APIController.cs
@@ -1,45 +1,42 @@
 using System;
 using Newtonsoft.Json;
 
-namespace Controller
+class APIController
 {
-    class APIController
+    public static async Task<double> APICall(string stockSymbol)
     {
-        public static async Task<double> APICall(string stockSymbol)
+        
+        var client = new HttpClient();
+        double stockValue = 0;
+        String jsonFile = File.ReadAllText("appsettings.json");
+        dynamic infoInJson = JsonConvert.DeserializeObject(jsonFile);
+
+        string apiKey = infoInJson["Smtp"]["Apikey"].Value;
+
+        var request = new HttpRequestMessage
         {
-            
-            var client = new HttpClient();
-            double stockValue = 0;
-            String jsonFile = File.ReadAllText("appsettings.json");
-            dynamic infoInJson = JsonConvert.DeserializeObject(jsonFile);
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://api.twelvedata.com/time_series?apikey={apiKey}&interval=1min&symbol={stockSymbol}&format=JSON&outputsize=1"),
+            // RequestUri = new Uri($"https://www.google.com/search?q={stockSymbol}&oq={stockSymbol}&ie=UTF-8"),
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            dynamic bodyInJson = JsonConvert.DeserializeObject(body);
 
-            string apiKey = infoInJson["Smtp"]["Apikey"].Value;
-
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://api.twelvedata.com/time_series?apikey={apiKey}&interval=1min&symbol={stockSymbol}&format=JSON&outputsize=1"),
-                // RequestUri = new Uri($"https://www.google.com/search?q={stockSymbol}&oq={stockSymbol}&ie=UTF-8"),
-            };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                dynamic bodyInJson = JsonConvert.DeserializeObject(body);
-
-                // verifica se vier com algum código diferente de 200 e retornar o erro antes de mandar o email
-                if( bodyInJson["status"].Value == "ok" ){
-                    // A divisão é uma forma de converter o valor que chega em formato string e é passado para double
-                    stockValue = Convert.ToDouble(bodyInJson["values"][0]["close"].Value)/100000;
-                }
-                else{
-
-                    // Caso alguém digite algo errado
-                    throw new ArgumentException($"Something Wrong Happened. CodeStatus:{bodyInJson["code"].Value}, {bodyInJson["message"].Value}.");
-                }
-
+            // verifica se vier com algum código diferente de 200 e retornar o erro antes de mandar o email
+            if( bodyInJson["status"].Value == "ok" ){
+                // A divisão é uma forma de converter o valor que chega em formato string e é passado para double
+                stockValue = Convert.ToDouble(bodyInJson["values"][0]["close"].Value)/100000;
             }
-            return stockValue;
+            else{
+
+                // Caso alguém digite algo errado
+                throw new ArgumentException($"Something Wrong Happened. CodeStatus:{bodyInJson["code"].Value}, {bodyInJson["message"].Value}.");
+            }
+
         }
+        return stockValue;
     }
 }
--- Controller/MailController.cs
+++ Controller/MailController.cs
@@ -9,76 +9,73 @@ using MimeKit;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Controller
-{
-    class MailController
-    { 
-        public static void MailCall( String MessageType )
+class MailController
+{ 
+    public static void MailCall( String MessageType )
+    {
+        MimeMessage message = new MimeMessage();
+        String jsonFile = File.ReadAllText("appsettings.json");
+        dynamic infoInJson = JsonConvert.DeserializeObject(jsonFile);
+        int port = 0;
+
+        String emailAddress = infoInJson["Smtp"]["Email"].Value.ToString();
+        String password = infoInJson["Smtp"]["Password"].Value.ToString() ;
+        // Array toWho = infoInJson["EmailConfiguration"]["toWho"].Value;
+        string smtpClient = infoInJson["Smtp"]["Host"].Value.ToString();
+        Int32.TryParse( infoInJson["Smtp"]["Port"].Value , out port );
+
+        message.From.Add(new MailboxAddress("Stock Warning", emailAddress));
+
+        if(infoInJson["EmailConfiguration"]["toWho"].GetType().ToString() == "Newtonsoft.Json.Linq.JArray"){
+            foreach (string email in infoInJson["EmailConfiguration"]["toWho"])
+            {
+                message.To.Add( MailboxAddress.Parse(email) );
+            }
+        }
+        else{
+            message.To.Add( MailboxAddress.Parse( infoInJson["EmailConfiguration"]["toWho"].Value ) );
+        }
+
+        
+        if ( MessageType == "Sell" )
         {
-            MimeMessage message = new MimeMessage();
-            String jsonFile = File.ReadAllText("appsettings.json");
-            dynamic infoInJson = JsonConvert.DeserializeObject(jsonFile);
-            int port = 0;
+            message.Subject = "Vende ai amigo";
+            message.Body = new TextPart("plain")
+            {
+                Text= @"Yes,
+                you can Sell!"
+            };
+        }
+        else
+        {
+            message.Subject = "Compra ai amigo";
+            message.Body = new TextPart("plain")
+            {
+                Text= @"Yes,
+                        you can Buyyy!"
+            };
+        }
 
-            String emailAddress = infoInJson["Smtp"]["Email"].Value.ToString();
-            String password = infoInJson["Smtp"]["Password"].Value.ToString() ;
-            // Array toWho = infoInJson["EmailConfiguration"]["toWho"].Value;
-            string smtpClient = infoInJson["Smtp"]["Host"].Value.ToString();
-            Int32.TryParse( infoInJson["Smtp"]["Port"].Value , out port );
-
-            message.From.Add(new MailboxAddress("Stock Warning", emailAddress));
-
-            if(infoInJson["EmailConfiguration"]["toWho"].GetType().ToString() == "Newtonsoft.Json.Linq.JArray"){
-                foreach (string email in infoInJson["EmailConfiguration"]["toWho"])
-                {
-                    message.To.Add( MailboxAddress.Parse(email) );
-                }
-            }
-            else{
-                message.To.Add( MailboxAddress.Parse( infoInJson["EmailConfiguration"]["toWho"].Value ) );
-            }
-
+        SmtpClient client = new SmtpClient();
+        try
+        {
+            client.Connect(smtpClient, port, true);
             
-            if ( MessageType == "Sell" )
-            {
-                message.Subject = "Vende ai amigo";
-                message.Body = new TextPart("plain")
-                {
-                    Text= @"Yes,
-                    you can Sell!"
-                };
-            }
-            else
-            {
-                message.Subject = "Compra ai amigo";
-                message.Body = new TextPart("plain")
-                {
-                    Text= @"Yes,
-                         you can Buyyy!"
-                };
-            }
+            client.Authenticate(emailAddress, password);
+            client.Send(message);
+            Console.WriteLine("Email Sent!");
+        }
+        catch (System.Exception ex)
+        {
+            
+            Console.Write(ex);
+            throw;
+        }
+        finally
+        {
+            client.Disconnect(true);
 
-            SmtpClient client = new SmtpClient();
-            try
-            {
-                client.Connect(smtpClient, port, true);
-                
-                client.Authenticate(emailAddress, password);
-                client.Send(message);
-                Console.WriteLine("Email Sent!");
-            }
-            catch (System.Exception ex)
-            {
-                
-                Console.Write(ex);
-                throw;
-            }
-            finally
-            {
-                client.Disconnect(true);
-
-                client.Dispose();
-            }
+            client.Dispose();
         }
     }
 }
